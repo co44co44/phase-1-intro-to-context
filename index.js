0600e1aa6418d7fbf890a9e41f3d0638d377bfe0
Maria Cristina Ordonez <crisordonez44@gmail.com>
2022-11-15 09:57:04 -0500

    commit tree f49896daa7ddd015a2f359c957f358a67c627052

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+function createEmployeeRecord(employeeArray){
+// let employeeCard = {firstName:"", familyName:"", title:"", payPerHour:[] , timeInEvents:[], timeOutEvents:[],}
+return {
+    firstName: employeeArray[0],
+    familyName: employeeArray[1],
+    title: employeeArray[2],
+    payPerHour: employeeArray[3],
+    timeInEvents: [],
+    timeOutEvents: []   
+    }
+};
+
+function createEmployeeRecords(arrayOfEmployeeArrays){
+    return arrayOfEmployeeArrays.map(function (employeeArray){
+        return createEmployeeRecord(employeeArray)
+    })
+};
+
+function createTimeInEvent(employeeArray, dateStamp){
+    dateStamp = new Date ("YYYY-MM-DD HHMM")
+    return createEmployeeRecord(employeeArray,dateStamp)
+}
+
+function createTimeOutEvent(){}
+
+function hoursWorkedOnDate(){}
+
+function wagesEarnedOnDate(){}
+
+function allWagesFor(){}
+
+function calculatePayroll(){}
+
